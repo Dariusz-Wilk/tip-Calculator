c8f091e983dfdc429bdacd50a87bf4904a0c6194
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-07-18 12:45:19 +0100

    add function checkForm and add addEventListener for button calling this function

--- js/script.js
+++ js/script.js
@@ -8,4 +8,17 @@
 	const errorMsg = document.getElementById('error');
 	const priceForOne = document.getElementById('cost');
 	const tipInfo = document.getElementById('tip-info');
+
+	const checkForm = function () {
+		if (priceToPay.value == 0 || numsOfPeople.value == 0) {
+			errorMsg.textContent = 'Wprowadź wszystkie dane!';
+		} else {
+			errorMsg.textContent = '';
+		}
+	};
+
+	countBtn.addEventListener('click', e => {
+		e.preventDefault();
+		checkForm();
+	});
 }
