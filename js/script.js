9f57c38411ebf62435b63a2cf77cf87a3f31168b
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-07-18 12:26:27 +0100

    get all need const from DOM

---
+++ js/script.js
@@ -0,0 +1,11 @@
+{
+	const priceToPay = document.getElementById('price');
+	const numsOfPeople = document.getElementById('people');
+	const tipToPay = document.getElementById('tip');
+
+	const countBtn = document.getElementById('count-btn');
+
+	const errorMsg = document.getElementById('error');
+	const priceForOne = document.getElementById('cost');
+	const tipInfo = document.getElementById('tip-info');
+}
